aa8f9ebc040057fd94f7b7f349bc19fae19993a3
George Sheng <gsheng112@gmail.com>
2021-06-30 16:23:47 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+let onscreen = [];
+let offscreen = [];
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+console.log(-26 > -175)
+document.addEventListener("scroll", (e) => {
+    onscreen = [];
+    offscreen = [];
+    var allElements = document.getElementsByTagName("*");
+    var allIds = [];
+    for (var i = 0, n = allElements.length; i < n; ++i) {
+        var el = allElements[i];
+        if (el.id) { allIds.push(el.id); }
+    }
+    for(let i = 0; i < allIds.length; i++){
+        var id = allIds[i];
+        if(isOnScreen(document.getElementById(id))){
+            onscreen.push(id);
+        }
+        else{
+            offscreen.push(id);
+        }
+    }
+});
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.addEventListener("click", (e) => {
+    console.log("On Screen: " + onscreen);
+    console.log("Off Screen: " + offscreen);
+    isOnScreen(document.getElementById("box3"));
+})
+
+function isOnScreen(element) {
+	var position = element.getBoundingClientRect();
+    var height = element.offsetHeight;
+
+
+
+	// checking whether fully visible
+	if(position.top >= 0 && position.bottom <= window.innerHeight) {
+		return true;
+	}
+
+	// checking for partial visibility
+	if(position.top > -1 * height/2 && position.bottom < window.innerHeight + height/2) {
+		return true;
+	}
+    return false;
+  }
